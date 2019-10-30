af80ba0ac99349b20ab5ec5a2254e841e3ed1e86
Robert Lin <robertlin1@gmail.com>
2019-10-29 21:17:45 -0700

    Initial action setup

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,71 @@
-import * as core from '@actions/core';
-import {wait} from './wait'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const { repo, ref, sha } = github.context;
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
+    const token = core.getInput('token', { required: true });
+    const step = core.getInput('step', { required: true });
+    const logsURL = core.getInput('logs', { required: false });
+    const description = core.getInput('desc', { required: true });
 
-    core.setOutput('time', new Date().toTimeString());
+    const client = new github.GitHub(token);
+    switch (step) {
+    case 'start':
+      {
+        const environment = core.getInput('env', { required: true });
+        const transient = core.getInput('desc', { required: false }) === 'true';
+
+        const deployment = await client.repos.createDeployment({
+          owner: repo.owner,
+          repo: repo.repo,
+          ref: ref,
+          required_contexts: [],
+          environment,
+          auto_merge: false,
+          transient_environment: transient,
+        });
+
+        const deploymentID = deployment.data.id.toString();
+        core.debug(`created deployment ${deploymentID}`);
+        core.setOutput('deployment_id', deploymentID);
+    
+        await client.repos.createDeploymentStatus({
+          ...repo,
+          deployment_id: deployment.data.id,
+          state: 'in_progress',
+          log_url: logsURL || `https://github.com/${repo.owner}/${repo.repo}/commit/${sha}/checks`,
+          description,
+        });
+
+        core.debug('deployment status set to "in_progress"');
+      }
+    case 'finish':
+      {
+        const deploymentID = core.getInput('deployment_id', { required: true });
+        const envURL = core.getInput('env-url', { required: true });
+        const status = core.getInput('status', { required: true });
+        if (status !== 'success' && status !== 'failure' && status !== 'cancelled') {
+          core.error(`unexpected status ${status}`);
+          return;
+        }
+
+        const newStatus = status === 'cancelled' ? 'inactive' : status;
+        await client.repos.createDeploymentStatus({
+          ...repo,
+          deployment_id: parseInt(deploymentID, 10),
+          state: newStatus,
+          log_url: logsURL || `https://github.com/${repo.owner}/${repo.repo}/commit/${sha}/checks`,
+          environment_url: envURL,
+          description,
+        });
+
+        core.debug(`${deploymentID} status set to ${newStatus}`);
+      }
+    default:
+      core.setFailed(`unknown step type ${step}`);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
