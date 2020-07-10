0add778f17bb05c39273830ed12ea5b57b3992f6
tallyb <tally.barak@gmail.com>
2020-07-10 10:23:21 +0300

    Fix main file

--- dist/index.js
+++ dist/index.js
@@ -403,6 +403,102 @@ module.exports =
 /******/ 		};
 /******/
 /******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete installedModules[moduleId];
+/******/ 		}
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	__webpack_require__.ab = __dirname + "/";
+/******/
+/******/ 	// the startup function
+/******/ 	function startup() {
+/******/ 		// Load entry module and return exports
+/******/ 		return __webpack_require__(429);
+/******/ 	};
+/******/
+/******/ 	// run startup
+/******/ 	return startup();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 16:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_2098__(16);
+
+/***/ }),
+
+/***/ 87:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_2098__(87);
+
+/***/ }),
+
+/***/ 129:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_2098__(129);
+
+/***/ }),
+
+/***/ 211:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_2098__(211);
+
+/***/ }),
+
+/***/ 357:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_2098__(357);
+
+/***/ }),
+
+/***/ 413:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_2098__(413);
+
+/***/ }),
+
+/***/ 429:
+/***/ (function(module, __unusedexports, __nested_webpack_require_2098__) {
+
+module.exports =
+/******/ (function(modules, runtime) { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/
 /******/ 		// Flag the module as loaded
@@ -9961,6 +10057,66 @@ function onceStrict (fn) {
   return f
 }
 
+
+/***/ })
+
+/******/ });
+
+/***/ }),
+
+/***/ 605:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_2098__(605);
+
+/***/ }),
+
+/***/ 614:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_2098__(614);
+
+/***/ }),
+
+/***/ 622:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_2098__(622);
+
+/***/ }),
+
+/***/ 631:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_2098__(631);
+
+/***/ }),
+
+/***/ 669:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_2098__(669);
+
+/***/ }),
+
+/***/ 747:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_2098__(747);
+
+/***/ }),
+
+/***/ 761:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_2098__(761);
+
+/***/ }),
+
+/***/ 835:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_2098__(835);
 
 /***/ })
 
