e3774edc2214215684456a423e23ccbab6951b5c
Asher Tuggle <asher@cast.app>
2022-04-28 08:58:54 -0600

    make delete env work

---
+++ src/lib/delete.ts
@@ -0,0 +1,40 @@
+import { GitHub } from "@actions/github/lib/utils";
+
+import { DeploymentContext } from "./context";
+import deactivateEnvironment from "./deactivate";
+
+/**
+ * Delete all deployments within this environment.
+ */
+async function deleteEnvironment(
+  github: InstanceType<typeof GitHub>,
+  context: DeploymentContext
+) {
+  const {
+    log,
+    owner,
+    repo,
+    coreArgs: { environment },
+  } = context;
+  const deployments = await deactivateEnvironment(github, context);
+  if (!deployments) {
+    return;
+  }
+  const existing = deployments.data.length;
+  for (let i = 0; i < existing; i++) {
+    const deployment = deployments.data[i];
+    log.info(
+      `${environment}.${deployment.id}: deleting deployment (${deployment.sha})"`
+    );
+    await github.rest.repos.deleteDeployment({
+      owner,
+      repo,
+      deployment_id: deployment.id,
+    });
+    log.debug(`${environment}.${deployment.id} deleted`);
+  }
+
+  log.info(`${environment}: ${existing} deployments deleted`);
+}
+
+export default deleteEnvironment;
